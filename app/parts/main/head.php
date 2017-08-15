a7aee421706ca063da7cee097641fd375ec4b3d1
user <brewbaker@yandex.com>
2017-08-15 20:37:08 +0300

    Create head.php

---
+++ app/parts/main/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <script src="https://use.fontawesome.com/fe69f16b44.js"></script>
+  <link rel="stylesheet" id="bulma" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.2/css/bulma.min.css" />
+  <link rel="stylesheet" type="text/css" href="https://l-o-g.000webhostapp.com/p/testlogin.css">
+  
+</head>
